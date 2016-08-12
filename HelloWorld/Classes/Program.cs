ab29654e73aad54b23fbb936b5f74423cfb6f24c
Konrad Kontek <vrado-dev@outlook.de>
2016-08-12 10:22:26 +0200

    Classes Section added

---
+++ HelloWorld/Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine( "My Name is " +FirstName + " "+ LastName);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+        }
+
+    }
+
+}
+
+
