d9923cab445e84469745a99f19ac91fb08001816
Konrad Kontek <vrado-dev@outlook.de>
2016-08-12 10:00:16 +0200

    TypeConversion and Cast finishes

--- HelloWorld/TypeConversion/Program.cs
+++ HelloWorld/TypeConversion/Program.cs
@@ -10,6 +10,20 @@ namespace TypeConversion
             byte b = 1;
             int i = b;
             Console.WriteLine(i);
+
+          
+
+            try
+            {
+                var number = "1234";
+                byte a = Convert.ToByte(number);
+                Console.WriteLine(a);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could nut be converted to a byte!");
+                throw;
+            }
         }
     }
 }
