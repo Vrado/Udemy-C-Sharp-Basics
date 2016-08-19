0532adf24cc7759c0103b5f3924048c42de8b08c
Konrad Kontek <vrado-dev@outlook.de>
2016-08-19 09:08:13 +0200

    Enums added

--- HelloWorld/Enums/Program.cs
+++ HelloWorld/Enums/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace Enums
+﻿using System;
+
+namespace Enums
 {
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
     internal class Program
     {
         private static void Main(string[] args)
         {
+            var method = ShippingMethod.Express;
+            Console.WriteLine(method);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod) Enum.Parse(typeof(ShippingMethod), methodName);
         }
     }
 }
