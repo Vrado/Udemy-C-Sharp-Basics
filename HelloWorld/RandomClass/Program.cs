521369fb2a93d609e24f0b760278cf23b8213231
Konrad Kontek <vrado-dev@outlook.de>
2016-08-25 09:50:40 +0200

    Added Random
    +Random Password generator

---
+++ HelloWorld/RandomClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RandomClass
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var random = new Random();
+            const int pwdlength = 10;
+            var buffer = new char[pwdlength];
+            for (var i = 0; i < pwdlength; i++)
+                buffer[i] = (char) ('a' + random.Next(0, 26));
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
+        }
+    }
+}
